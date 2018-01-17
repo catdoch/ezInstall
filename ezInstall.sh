091141d11dbc12b7522ac133364228922bd4f6c6
Cat Docherty <cdocherty@ebay.com>
2018-01-17 11:50:21 +0000

    Initial bash script commit

---
+++ ezInstall.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+brew cask install spotify
+brew cask install intellij-idea
+brew cask install visual-studio-code
+brew cask install sublime-text
+brew cask install dwihn0r-keepassx
+brew cask install slack
+brew cask install firefox
