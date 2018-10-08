abb11d8b656ac2bccf84724f947d1d974a68f675
CDocherty <cdocherty@ebay.com>
2018-10-08 10:19:20 +0100

    [chore(*)]: add options to install

--- ezInstall.sh
+++ ezInstall.sh
@@ -1,9 +1,63 @@
 #!/bin/sh
 
-brew cask install spotify
-brew cask install intellij-idea
-brew cask install visual-studio-code
-brew cask install sublime-text
-brew cask install dwihn0r-keepassx
-brew cask install slack
-brew cask install firefox
+pretty_print() {
+  printf "\n%b\n" "[Install-bot] $1"
+}
+
+pretty_print "Hey! Wanna install some stuff?"
+
+while true; do
+    read -p "Have you already installed homebrew? " yn
+    case $yn in
+        [Yy]* ) echo "Great! Let's get installing"; break;;
+        [Nn]* ) echo "Womp womp - you have to install it first"; exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+while true; do
+    read -p "Do you wanna install VSCode? " yn
+    case $yn in
+        [Yy]* ) brew cask install visual-studio-code; break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+while true; do
+    read -p "Do you wanna install Sublime text? " yn
+    case $yn in
+        [Yy]* ) brew cask install sublime-text; break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+while true; do
+    read -p "Do you wanna install Slack? " yn
+    case $yn in
+        [Yy]* ) brew cask install slack; break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+while true; do
+    read -p "Do you wanna install Firefox? " yn
+    case $yn in
+        [Yy]* ) brew cask install firefox; break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+while true; do
+    read -p "Do you wanna install Spotify? " yn
+    case $yn in
+        [Yy]* ) brew cask install spotify; break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+pretty_print "That's all I got for now, goodbye!"
